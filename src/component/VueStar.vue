ac97dd332a62fa82c31c0aa655f9df7c6bffb75d
Yibin Chen <chenyibin@qtone.cn>
2018-06-20 11:43:34 +0800

    add active unCancel attr and add toggle methods

--- src/component/VueStar.vue
+++ src/component/VueStar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="VueStar"> 
+  <div class="VueStar">
     <div class="VueStar__ground">
       <div class="VueStar__icon" @click="toggle" :class="AnimateClass" :style='{color:ColorValue}'>
         <slot name="icon"></slot>
@@ -10,47 +10,59 @@
 </template>
 
 <script>
-import { isColors } from '../uitls/colorRE.js'
-export default {
-  name: 'VueStar',
-  props: {
-    animate: String,
-    color: String
-  },
-  methods: {
-    toggle () {
-      this.active = !this.active
-      this.toggleAnimate = !this.toggleAnimate
-      this.toggleColor = !this.toggleColor
+    import { isColors } from '../../uitls/colorRE.js'
+    export default {
+        name: 'VueStar',
+        props: {
+            animate: String,
+            color: String,
+            active:Boolean,
+            unCancel:Boolean,
+            index:Number,
+        },
+        methods: {
+            toggle () {
+                if(this.unCancel){
+                    if(!this.active){
+                        this.active = !this.active
+                        this.toggleAnimate = !this.toggleAnimate
+                        this.toggleColor = !this.toggleColor
+                        this.$emit('toggle',this.active,this.index);
+                    }
+                }else{
+                    this.active = !this.active
+                    this.toggleAnimate = !this.toggleAnimate
+                    this.toggleColor = !this.toggleColor
+                    this.$emit('toggle',this.active,this.index);
+                }
+            }
+        },
+        data () {
+            return {
+                toggleAnimate: false,
+                toggleColor: false
+            }
+        },
+        computed: {
+            AnimateClass () {
+                return this.toggleAnimate||this.active ? this.animate : ''
+            },
+            ColorValue () {
+                return this.toggleColor||this.active ? this.color : ''
+            }
+        },
+        mounted () {
+            if (this.color) {
+                if (isColors(this.color)) {
+                    return
+                } else {
+                    console.error('this color must be hexcolor or rgbcolor  ---VueStar')
+                }
+            } else {
+                return
+            }
+        }
     }
-  },
-  data () {
-    return {
-      active: false,
-      toggleAnimate: false,
-      toggleColor: false
-    }
-  },
-  computed: {
-    AnimateClass () {
-      return this.toggleAnimate ? this.animate : ''
-    },
-    ColorValue () {
-      return this.toggleColor ? this.color : ''
-    }
-  },
-  mounted () {
-    if (this.color) {
-      if (isColors(this.color)) {
-        return
-      } else {
-        console.error('this color must be hexcolor or rgbcolor  ---VueStar')
-      }
-    } else {
-      return
-    }
-  }
-}
 </script>
 
 <style lang="less">
